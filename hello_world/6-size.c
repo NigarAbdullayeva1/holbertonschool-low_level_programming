177fa81bc10807dc9db43aa589781793db372b43
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-02-20 01:29:44 -0800

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
     printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
     printf("Size of a int:%lu byte(s)\n",(unsigned long)sizeof(i));
-    printf("Size of a int long:%lu byte(s)\n",(unsigned long)sizeof(il));
-    printf("Size of a int long long:%lu byte(s)\n",(unsigned long)sizeof(ill));
+    printf("Size of a long int:%lu byte(s)\n",(unsigned long)sizeof(il));
+    printf("Size of a long long int:%lu byte(s)\n",(unsigned long)sizeof(ill));
     printf("Size of a float:%lu byte(s)\n",(unsigned long)sizeof(f));
     return (0);
 }
