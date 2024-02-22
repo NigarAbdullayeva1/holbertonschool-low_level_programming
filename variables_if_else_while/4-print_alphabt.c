d749808d68f209c5110701d5b31820eaa14c4405
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-02-22 10:02:56 +0000

    4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the alphabet in lowercase, except for q and e.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	char c;
-	
-	for (c = 'a'; c <= 'z'; c++)
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if (c != 'q' && c != 'e')
-			putchar(c);
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
 	}
 
 	putchar('\n');
 
-        return (0);
+	return (0);
 }
