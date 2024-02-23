8d35080f95d8a073f9f5c29f6a9cee093d1d302a
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-02-23 12:39:49 +0000

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	char c;
+
+	for (a = 0; a < 10; a++)
+		putchar (a + '0');
+	for (c = 'a'; c <= 'f'; c++)
+		putchar (c);
+	
+	putchar('\n');
+	
+	return (0);
+}
