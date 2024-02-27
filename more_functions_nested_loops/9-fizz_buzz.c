3f82897ae85112115de5a6f40e54bf298a3cdc50
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-02-27 22:40:01 +0000

    9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * FizzBuzz - print from 1 to 100
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 1; i <= 100; i++)
+	{
+		if(i % 3 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+		}
+		else if(i % 5 == 0)
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+                }
+		else if(i % 5 == 0)
+                {
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar(i + '0');
+			_putchar(' ');
+		}
+	}
+	return (0);
+}
