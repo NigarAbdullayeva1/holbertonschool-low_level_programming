b98d42d3432f7210c836c14f693474e1da491ad4
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-02-27 21:36:19 +0000

    8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,12 +2,16 @@
 
 /**
  * print_square -prints a square.
+ *@size: size of the square
  *
  * Return: void.
  */
 void print_square(int size)
 {
 	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
 
 	for (i = 0; i < size; i++)
 	{
