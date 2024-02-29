2dd096b61c8f6cc44562540e1b3f04a5a956aaf3
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-02-29 21:45:23 +0000

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - a function that prints a sentence followed by new line
+ * @str: input
+ * Return: (i)
+ */
+void _puts(char *s)
+{
+  puts(s);
+}
