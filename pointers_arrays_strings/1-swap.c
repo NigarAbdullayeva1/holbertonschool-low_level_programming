405fb4f699644b81a887e8b515253191f5884221
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-02-29 08:53:46 +0000

    1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -2,15 +2,14 @@
 
 /**
  * swp_int -  function that swaps the values of two integers.
- * @a - pointer a
- * @b - pointers b
- *
+ * @a - pointer
+ * @b - pointer
  * return: void/
  */
 void swap_int(int *a, int *b)
 {
 	int tmp;
-	
+
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
