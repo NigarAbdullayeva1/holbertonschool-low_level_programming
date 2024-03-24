d9477e36ee1052a6e51d01bfb6fa43d6987bb632
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-03-24 12:57:58 +0000

    2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,5 +14,5 @@ void print_dog(struct dog *d)
 		(*d).name = "(nil)";
 		(*d).owner = "(nil)";
 	}
-	printf("Name: %s\nAge: %1f\nOwner: %s\n", (*d).name,(*d).age, (*d).owner);
+	printf("Name: %s\nAge: %1f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 }
