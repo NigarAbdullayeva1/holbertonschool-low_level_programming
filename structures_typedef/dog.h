462079cd78093e28762e94afc26de8dcb1711f02
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-03-24 10:21:06 +0000

    0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for struct
+ * @name: char
+ * @age: float
+ * @owner: char
+ * Description: description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dogs;
+
+#endif
